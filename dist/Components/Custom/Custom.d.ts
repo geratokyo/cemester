5b97f8ed37728858b143ee02d7c268967d84a932
Falcon <gera.kourtidis@gmail.com>
2019-07-04 12:42:32 +0100

    remove custom component, this is now managed from customcomponentservice

--- dist/Components/Custom/Custom.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { iCmsItem, iCmsProps } from '../../models';
-export interface CustomProps extends iCmsProps {
-    className?: string;
-    item: iCmsItem;
-}
-export declare const Custom: React.SFC<CustomProps>;
