c69deabd9cac0a43c5f9a0d28fd7b5840d354cef
Falcon <gera.kourtidis@gmail.com>
2019-07-05 12:47:47 +0100

    remove springscroll component

--- src/Components/SinglePage/SinglePage.tsx
+++ src/Components/SinglePage/SinglePage.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { SpringScroll } from '../SpringScroll/SpringScroll';
 import Scrollbars from 'react-custom-scrollbars';
 import { iCmsItem, iPageProps, Dictionary } from '../../models';
-import { Section } from '../Section/Section';
+import Section, {Section as ClassSection} from '../Section/Section';
 
 export interface SinglePageProps extends iPageProps {
     className?: string;
@@ -16,7 +16,7 @@ export interface SinglePageState {
 export class SinglePage extends React.Component<SinglePageProps, SinglePageState>{
     topEl: HTMLElement;
     el: HTMLDivElement;
-    elements: Dictionary<Section> = {};
+    elements: Dictionary<ClassSection|any> = {};
     scrollEl: Scrollbars;
     windowHeight: number;
     constructor(p: SinglePageProps) {
@@ -56,10 +56,10 @@ export class SinglePage extends React.Component<SinglePageProps, SinglePageState
     }
 
     handleScroll = (e) => {
-        this.checkTopNavBar()
-        this.setState({
-            offset: e.scrollTop
-        })
+        // this.checkTopNavBar()
+        // this.setState({
+        //     offset: e.scrollTop
+        // })
     }
 
     checkTopNavBar = () => {
@@ -78,11 +78,11 @@ export class SinglePage extends React.Component<SinglePageProps, SinglePageState
                 {
                     (state.isNavFixed && props.header) &&
                     <div className="sp__nav sp__nav--static"
-                        style={{ 
-                            position: "absolute", 
-                            width: "100%", 
-                            height: "auto", 
-                            zIndex: 9999 
+                        style={{
+                            position: "absolute",
+                            width: "100%",
+                            height: "auto",
+                            zIndex: 9999
                         }}
                     >
                         {
@@ -96,38 +96,31 @@ export class SinglePage extends React.Component<SinglePageProps, SinglePageState
                         }
                     </div>
                 }
-                <SpringScroll ref={e => this.scrollEl = e}
-                    className={"sp__spring-scroll"}
-                    style={{
-                        height: "100vh"
-                    }}
-                    onScrollFrame={this.handleScroll}>
-                    {
-                        (!state.isNavFixed && props.header) &&
-                        <div className="sp__nav">
-                            {
-                                React.Children.map(this.props.header, (child: React.ReactElement<any>) => {
-                                    return React.cloneElement(child, {
-                                        ...child.props,
-                                        scrollTo: this.scrollToSection
-                                    })
-                                })[0]
-                            }
-                        </div>
-                    }
-                    <div className="sp__content"
-                        ref={e => this.topEl = e}>
+                {
+                    (!state.isNavFixed && props.header) &&
+                    <div className="sp__nav">
                         {
-                            props.content.map((e: iCmsItem) => <Section
-                                ref={ee => { this.elements[e.key] = ee }}
-                                key={e.key}
-                                offset={state.offset}
-                                item={e}
-                                screenThreshold={this.props.screenThreshold}
-                            />)
+                            React.Children.map(this.props.header, (child: React.ReactElement<any>) => {
+                                return React.cloneElement(child, {
+                                    ...child.props,
+                                    scrollTo: this.scrollToSection
+                                })
+                            })[0]
                         }
                     </div>
-                </SpringScroll>
+                }
+                <div className="sp__content"
+                    ref={e => this.topEl = e}>
+                    {
+                        props.content.map((e: iCmsItem) => <Section
+                            ref={ee => { this.elements[e.key] = ee }}
+                            key={e.key}
+                            offset={state.offset}
+                            item={e}
+                            screenThreshold={this.props.screenThreshold}
+                        />)
+                    }
+                </div>
             </div>
         )
     }
