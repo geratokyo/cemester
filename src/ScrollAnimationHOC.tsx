35820074f253c5683deda6b161b05b2e04d0ad64
Falcon <gera.kourtidis@gmail.com>
2019-07-05 12:44:07 +0100

    add scroll animation

---
+++ src/ScrollAnimationHOC.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'; 
+import ScrollAnimation from './ScrollAnimation' ;
+import { iCmsItem, iCmsAttributes } from './models';
+
+
+export interface ScrollAnimationHOCProps{
+
+}
+
+export const ScrollAnimationHOC= <P extends Object>(WrappedComponent:React.ComponentType<P>)=>{
+    return class ScrollAnimationHOC extends React.Component<P, any>{
+        render(){
+            let {...props} = this.props as P;
+            let attr:iCmsAttributes = (this.props as any).item.attributes;
+
+            if(!attr || ((attr.animateIn == "" || !attr.animateIn) && (attr.animateOut == "" || !attr.animateOut))){
+                return <WrappedComponent {...props} />
+            }
+            return (
+                <ScrollAnimation {...attr}>
+                    <WrappedComponent {...props} />
+                </ScrollAnimation>
+            )
+        }
+    }
+}
+
+export default ScrollAnimationHOC; 
