8b1dd674d086f29eadd0a4bdf2466031daae7fb6
Falcon <gera.kourtidis@gmail.com>
2019-07-05 12:46:10 +0100

    export default components

--- src/Components.tsx
+++ src/Components.tsx
@@ -1,8 +1,18 @@
+export {SinglePage}  from "./Components/SinglePage/SinglePage"; 
 
-export { Container } from './Components/Container/Container';
-export { DivComponent } from './Components/DivComponent/DivComponent';
-export { Text } from './Components/Text/Text';
-export { Image } from './Components/Image/Image';
-export { Section } from './Components/Section/Section';
-export { SinglePage } from "./Components/SinglePage/SinglePage"; 
-export { SingleSectionPage } from './Components/SingleSectionPage/SingleSectionPage'; 
+export {SingleSectionPage}  from './Components/SingleSectionPage/SingleSectionPage'; 
+
+import Section from './Components/Section/Section';
+export {Section};
+
+import Container  from './Components/Container/Container';
+export { Container }
+
+import DivComponent  from './Components/DivComponent/DivComponent';
+export { DivComponent }
+
+import Text  from './Components/Text/Text';
+export { Text }
+
+import Image  from './Components/Image/Image';
+export { Image }
