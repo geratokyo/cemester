c2ce93c76c2097eb6f583403c78641b71e585449
Falcon <gera.kourtidis@gmail.com>
2019-07-05 12:48:52 +0100

    remove component function states

--- src/Components/Section/Section.tsx
+++ src/Components/Section/Section.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import { iCmsItem, iCmsProps, DEFAULTS } from '../../models';
 import { Content } from '../../Content';
-import  ScrollableAnchor, { configureAnchors } from "react-scrollable-anchor"; 
+import ScrollAnimation from '../../ScrollAnimation';
+import ScrollAnimationHocHOC from '../../ScrollAnimationHOC';
+// import ScrollableAnchor, { configureAnchors } from "react-scrollable-anchor";
 
 
 export interface SectionProps extends iCmsProps {
     className?: string;
     offset?: number;
     item: iCmsItem;
-    screenThreshold?:number; 
+    screenThreshold?: number;
 }
 
 export interface SectionState {
@@ -27,73 +29,38 @@ export class Section extends React.Component<SectionProps, SectionState>{
         }
     }
 
-    public static defaultProps:Partial<SectionProps> = {
-        screenThreshold:DEFAULTS.SCREEN_THRESHOLD
-    }
-
-    componentDidMount(){
-        configureAnchors({})
-        this.setState({
-            isInView: this.isInView()
-        })
-    }
-
-    componentDidUpdate(prevProps: SectionProps, prevState: SectionState) {
-        let isVisible = this.isInView();
-
-        if (prevProps.offset !== this.props.offset) {
-            this.setState({
-                isInView: isVisible
-            })
-        }
-        if (!prevState.isInView && this.state.isInView) {
-            this.setState({
-                isAnimated: true
-            })
-        }
-    }
-
-    shouldComponentUpdate(nextProps:SectionProps, nextState:SectionState){
-        if(nextState.isAnimated){
-            return false;
-        }
-        return true;
-    }
-
-    isInView = () => {
-        let cc = this.el.getBoundingClientRect();
-        let heightSplit = window.innerHeight * this.props.screenThreshold;
-        return cc.top >= 0 && cc.top < heightSplit
+    public static defaultProps: Partial<SectionProps> = {
+        screenThreshold: DEFAULTS.SCREEN_THRESHOLD
     }
 
     shouldShowChildren = () => {
         return this.props.item.children && this.props.item.children.length;
     }
 
-    getHtmlElement = ()=>{
+    getHtmlElement = () => {
         return this.el;
     }
 
     render() {
         let { props, state } = this;
-        let itemClass = props.item.className || "";
-        console.log("hehehe", ScrollableAnchor);
+        let {item} = props; 
+        let itemClass = item.attributes && item.attributes.className || "";
+
         return (
-            
             <section ref={e => this.el = e} className={`${itemClass}`}>
-                <ScrollableAnchor key={props.item.key} id={props.item.key}>
                 {
                     this.shouldShowChildren() &&
-                    props.item.children.map((e) => <Content
+                    item.children.map((e) => <Content
                         key={e.key}
                         offset={props.offset}
                         cmsOptions={{ ...props, isInView: state.isAnimated || state.isInView }}
                         item={e} />)
                 }
-                </ScrollableAnchor>
             </section>
         )
     }
 }
 
+
+export default ScrollAnimationHocHOC(Section);
 
