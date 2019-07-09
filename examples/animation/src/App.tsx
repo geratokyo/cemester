2386439037294a76110b9d2979d8976af6ed85b5
Falcon <gera.kourtidis@gmail.com>
2019-07-09 16:17:37 +0100

    fix header

--- examples/animation/src/App.tsx
+++ examples/animation/src/App.tsx
@@ -12,23 +12,21 @@ class App extends React.Component<any, any>{
             <div className="App"
                 ref={e => this.cemesterEl = e}
             >
+                <header>
+                    <ul>
+                        <li>
+                            <AnchorLink href="#first">First</AnchorLink>
+                        </li>
+                        <li>
+                            <AnchorLink href="#second">Second</AnchorLink>
+                        </li>
+                        <li>
+                            <AnchorLink href="#third">Third</AnchorLink>
+                        </li>
+                    </ul>
+                </header>
                 <Cemester
                     content={CONTENT}
-                    header={
-                        <header>
-                            <ul>
-                                <li>
-                                    <AnchorLink href="#first">First</AnchorLink>
-                                </li>
-                                <li>
-                                    <AnchorLink href="#second">Second</AnchorLink>
-                                </li>
-                                <li>
-                                    <AnchorLink href="#third">Third</AnchorLink>
-                                </li>
-                            </ul>
-                        </header>
-                    }
                     customComponents={(e) => {
                         switch (e.type) {
                             case "Hr":
@@ -214,6 +212,7 @@ const CONTENT = [{
                             "animateIn": "fadeInDown",
                             "offset": 200,
                             "animateOut": "fadeOutDown",
+                            "afterAnimatedIn":()=>{console.log("hahha")}
                             
                         },
                         "content": "<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>",
